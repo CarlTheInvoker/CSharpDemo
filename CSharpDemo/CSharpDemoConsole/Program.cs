0c7a39c598d463f0e712d7cb35c944e51c24eb0b
FAREAST\xiangjli <xiangjli@microsoft.com>
2021-01-20 16:57:32 +0800

    Revert "remove program file"
    This reverts commit 62d0f9e8c6b3b115f70fbe704ee4fb911f179071.

---
+++ CSharpDemo/CSharpDemoConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpDemoConsole
+{
+    using CSharpDemo;
+    using CSharpDemo.TaskParallelLibrary;
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IDemo demo = new WaitAwaitThreadIdDemo();
+            Console.WriteLine($"Demo {demo.GetType().Name} started.");
+
+            demo.RunDemo();
+
+            Console.WriteLine($"Demo {demo.GetType().Name} finished.");
+            Console.ReadKey();
+        }
+    }
+}
